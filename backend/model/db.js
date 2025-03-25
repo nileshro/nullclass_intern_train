a458ccaff9720d1c2c30c689d955415b121b7657
Nilesh Rokade <nileshrokade266@gmail.com>
2025-03-26 00:11:19 +0530

    Added project files

---
+++ backend/model/db.js
@@ -0,0 +1,8 @@
+
+const mongoose=require("mongoose")
+require('dotenv').config()
+DATABASE=process.env.DATABASEURL
+const url=DATABASE
+module.exports.connect=()=>{
+    mongoose.connect(url,console.log("Database is connected"))
+}
